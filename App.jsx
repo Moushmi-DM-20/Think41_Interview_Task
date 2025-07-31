c82fb1e4624a38dcb1e48bceace7121447689833
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-31 15:25:26 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProductsGrid from './components/ProductsGrid';
-import ProductDetails from './components/ProductDetail';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ProductsGrid />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
